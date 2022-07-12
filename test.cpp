84116ccbd2181e2d4bbb79193e9bdad589334c61
bentowski <bbaudry@student.42.fr>
2022-07-12 17:32:55 +0200

    ConnexionFile

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <fstream>
+
+int main()
+{
+	std::ofstream testfile("ConnexionFile");
+
+	testfile << "CAP LS" << "\r\n";
+	testfile << "pass pass" << "\r\n";
+	testfile << "nick surLeFil" << "\r\n";
+	testfile << "user Ivan Antoine localhost :Ben Antoine Ivan" << "\r\n";
+}
