8eb9d950b9f4301d45eb0efa5bb3c01eafdd20fd
bentowski <bbaudry@student.42.fr>
2022-07-13 07:48:26 +0200

    clear ft_run

--- main.cpp
+++ main.cpp
@@ -7,9 +7,7 @@ using namespace irc;
 void	sighand(int sig)
 {
 	if (sig == SIGINT)
-	{
 		exit(0);
-	}
 }
 
 bool check_buffer(string buf)
@@ -17,16 +15,18 @@ bool check_buffer(string buf)
 	size_t x = 0;
 	while (x < buf.size())
 	{
-		//cout << "is print : " << isprint(buf[x]) << " : " << buf[x] << endl;
-		if (!isprint(buf[x]) && buf[buf.size() - 1] != '\n')
-			if (buf[buf.size() - 2] != '\r')
-			{
-				//cout << "FAAAALSE" << endl;
+		if (!isprint(buf[x]) && buf[buf.size() - 1] != '\n' && buf[buf.size() - 2] != '\r')
 				return false;
-			}
 		x++;
 	}
 	return true;
+}
+
+void exit_error(string msg, Server & serv)
+{
+	cout << msg << endl;
+	close(serv.getFdServer());
+	exit(1);
 }
 
 void ft_run(int port, string password)
@@ -34,7 +34,7 @@ void ft_run(int port, string password)
 	char buffer[512];
 	string save = "";
 	Server serv(port, password);
-	int poll_ret, fd, test, receive;
+	int poll_ret, fd, creation, receive;
 	int fd_count = 1;
 	struct pollfd			_poll[1025];
 	_poll[0].fd = serv.getFdServer();
@@ -44,19 +44,14 @@ void ft_run(int port, string password)
 
 	while (serv.getState())
 	{
-		test = 0;
-		poll_ret = poll(_poll, fd_count, -1);
+		creation = 0;
 
-		if (poll_ret < 0)
-		{
-			perror("Select failed :");
-			break ;
-		}
+		if ((poll_ret = poll(_poll, fd_count, -1)) < 0)
+			exit_error("poll failed :", serv);
 		if ((poll_ret > 0))
 		{
 			for (int x = 0; x < fd_count; x++)
 			{
-
 				bzero(buffer, 512);
 				if (_poll[x].revents & POLLIN)
 				{
@@ -67,77 +62,60 @@ void ft_run(int port, string password)
 						{
 							_poll[fd_count].fd = fd;
 							_poll[fd_count].events = POLLIN;
-							test = 1;
+							creation = 1;
 							serv.setUpFdMax(fd);
 							fd_count++;
 						}
 						else
-						{
-							perror("Accept failed: ");
-							break;
-						}
+							exit_error("Accept failed: ", serv);
 					}
 					if ((receive = recv(fd, &buffer, 255, 0)) >= 1)
 					{
-						//cout << "BUFFER RECEIVE = " << buffer << endl;
-
-							if (buffer[strlen(buffer) - 1] == '\n')
+						if (buffer[strlen(buffer) - 1] == '\n')
+						{
+							save += buffer;
+							if (check_buffer(save))
 							{
-								//cout << "=============ENVOI COMMANDE===============" << endl;
-								save += buffer;
-								if (check_buffer(save))
-								{
-									serv.parse_buffer_command(save);
-									serv.execCommand(fd);
-									serv.welcome(fd);
-								}
-								else
-								{
-									//cout << "OOOOOOOKKKKKKKK" << endl;
-									serv.ft_error(*serv.getUser(fd), ERR_ERRSYNTAX, "");
-								}
-								save.clear();
+								serv.parse_buffer_command(save);
+								serv.execCommand(fd);
+								serv.welcome(fd);
 							}
 							else
-							{
-								save += buffer;
-								//cout << "in wait : " << save << endl;
-							}
-
+								serv.ft_error(*serv.getUser(fd), ERR_ERRSYNTAX, "");
+							save.clear();
+						}
+						else
+							save += buffer;
 					}
-					if (test != 1 && (receive < 1 || (*(serv.getUser(fd)))->getToClose() == 1))
+					if (creation != 1 && (receive < 1 || (*(serv.getUser(fd)))->getToClose() == 1))
 					{
-						//cout << "CLOSE FD" << endl;
 						_poll[x] = _poll[fd_count - 1];
 						_poll[x].events = POLLIN;
 						serv.deleteUser(serv.getUser(fd));
 						close(fd);
 						fd_count--;
 					}
-					if (test == 1)
+					if (creation == 1)
 						break ;
 				}
 			}
 		}
 		else
-			perror("There were select failures: ");
+			exit_error("There were poll failures: ", serv);
 	}
-	//cout << "finallllll" << endl;
 	close(serv.getFdServer());
 }
 
 int main(int argc, char **argv)
 {
 	int port;
-	if (argc == 3) //with password
+	if (argc == 3)
 	{
 		string password = argv[2];
 		if ((port = atoi(argv[1])) > 0)
 			ft_run(port, password);
 	}
 	else
-	{
 		cout << "error wrong number arguments" << endl;
-	}
 	return 0;
 }
